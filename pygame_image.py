e486ea2014bc0127a12c7083434620b2ed4ab2ff
takeru <c0b23078ca@edu.teu.ac.jp>
2024-06-11 17:28:30 +0900

    練習10:move_ipメソッド使用回数の変更

--- pygame_image.py
+++ pygame_image.py
@@ -26,16 +26,27 @@ def main():
         screen.blit(bg_img, [-x+3200, 0])
         screen.blit(bg_img2, [-x+4800, 0])
         key_list = pg.key.get_pressed() # 全キーの押した状態を取得
+        a = -1
+        b = 0
         if key_list[pg.K_UP]:
-            kk_rct.move_ip(0, -1)
+            b = -1
         if key_list[pg.K_DOWN]:
-            kk_rct.move_ip(0, +1)
+            b = +1
         if key_list[pg.K_LEFT]:
-            kk_rct.move_ip(-1, 0)
+            a = -2
         if key_list[pg.K_RIGHT]:
-            kk_rct.move_ip(+2, 0)
-        else:
-            kk_rct.move_ip(-1, 0)
+            a = +2        
+        kk_rct.move_ip((a, b))
+        # if key_list[pg.K_UP]:
+        #     kk_rct.move_ip(0, -1)
+        # if key_list[pg.K_DOWN]:
+        #     kk_rct.move_ip(0, +1)
+        # if key_list[pg.K_LEFT]:
+        #     kk_rct.move_ip(-1, 0)
+        # if key_list[pg.K_RIGHT]:
+        #     kk_rct.move_ip(+2, 0)
+        # else:
+        #     kk_rct.move_ip(-1, 0)
         screen.blit(kk_ing, kk_rct) #kk_imgをkk_rectの設定にしたがって貼り付ける
         pg.display.update()
         tmr += 1        
