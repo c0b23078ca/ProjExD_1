d0e7817ced83c56948624d26b339eedac0a76ab1
takeru <c0b23078ca@edu.teu.ac.jp>
2024-06-11 16:45:09 +0900

    練習8:押したキーに合わせてこうかとんを移動させる

--- pygame_image.py
+++ pygame_image.py
@@ -13,6 +13,8 @@ def main():
     bg_img2 = pg.transform.flip(bg_img, True, False)
     kk_ing = pg.image.load("fig/3.png")
     kk_ing = pg.transform.flip(kk_ing, True, False)
+    kk_rct = kk_ing.get_rect() #こうかとんRectの抽出
+    kk_rct.center = 300, 200
     tmr = 0
     while True:
         for event in pg.event.get():
@@ -23,8 +25,15 @@ def main():
         screen.blit(bg_img2, [-x+1600, 0])
         screen.blit(bg_img, [-x+3200, 0])
         screen.blit(bg_img2, [-x+4800, 0])
-        kk_rct = kk_ing.get_rect() #こうかとんRectの抽出
-        kk_rct.center = 300, 200
+        key_list = pg.key.get_pressed() # 全キーの押した状態を取得
+        if key_list[pg.K_UP]:
+            kk_rct.move_ip(0, -1)
+        if key_list[pg.K_DOWN]:
+            kk_rct.move_ip(0, +1)
+        if key_list[pg.K_LEFT]:
+            kk_rct.move_ip(-1, 0)
+        if key_list[pg.K_RIGHT]:
+            kk_rct.move_ip(+1, 0)
         screen.blit(kk_ing, kk_rct) #kk_imgをkk_rectの設定にしたがって貼り付ける
         pg.display.update()
         tmr += 1        
