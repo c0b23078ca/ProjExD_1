5834a719637e6e9f79204f74d9786927ffd26a2a
takeru <c0b23078ca@edu.teu.ac.jp>
2024-06-11 16:15:30 +0900

    練習:こうかとんの表示

--- pygame_image.py
+++ pygame_image.py
@@ -12,13 +12,16 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_ing = pg.image.load("fig/3.png")
     kk_ing = pg.transform.flip(kk_ing, True, False)
-    
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(bg_img, [0,0])
+        kk_rct = kk_ing.get_rect() #こうかとんRectの抽出
+        kk_rct.center = 300, 200
+        screen.blit(kk_ing, kk_rct) #kk_imgをkk_rectの設定にしたがって貼り付ける
         pg.display.update()
         tmr += 1        
         clock.tick(10)
